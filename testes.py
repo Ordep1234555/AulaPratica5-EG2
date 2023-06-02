8880332615c89aff34e96a0e81888d74d44214ad
Pedro Mucci <48034949+Ordep1234555@users.noreply.github.com>
2023-06-02 18:35:00 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,31 +1,34 @@
+import unittest
 from calculator import MemoryCalculator
 
-def test_sum_is_zero_on_initialization():
-    calculator = MemoryCalculator()
-    assert calculator.sum() == 0
+class TestMemoryCalculator(unittest.TestCase):
 
-def test_sum_one_number():
+  def test_sum_is_zero_on_initialization(self):
+    calculator = MemoryCalculator()
+    self.assertEqual(0, calculator.sum())
+
+  def test_sum_one_number(self):
     calculator = MemoryCalculator()
     calculator.add(5)
-    assert calculator.sum() == 5
+    self.assertEqual(5, calculator.sum())
 
-def test_sum_two_numbers():
+  def test_sum_two_numbers(self):
     calculator = MemoryCalculator()
     calculator.add(5)
     calculator.add(10)
-    assert calculator.sum() == 15
+    self.assertEqual(15, calculator.sum())
 
-def test_sum_is_zero_after_calling_sum():
+  def test_sum_is_zero_after_calling_sum(self):
     calculator = MemoryCalculator()
     calculator.add(5)
-    calculator.sum()  # resultado: 5
-    assert calculator.sum() == 0
+    calculator.sum() # resultado: 5
+    self.assertEqual(0, calculator.sum())
 
-def test_sum_numbers_and_save_last_sum():
+  def test_sum_numbers_and_save_last_sum(self):
     calculator = MemoryCalculator()
     calculator._save_last_sum = True
     calculator.add(5)
     calculator.add(10)
     calculator.sum()
     calculator.add(15)
-    assert calculator.last_sum + calculator.sum() == 30
+    self.assertEqual(30, calculator.last_sum + calculator.sum())
