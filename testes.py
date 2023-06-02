5d2b56fc0b8866efe233a020c434de974c3bc4f3
Pedro Mucci <48034949+Ordep1234555@users.noreply.github.com>
2023-06-02 18:31:35 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,17 +1,31 @@
-import pytest
-from main import calcular_media
+from calculator import MemoryCalculator
 
-def test_lista_vazia():
-    numeros = []
-    resultado = calcular_media(numeros)
-    assert resultado == 0
+def test_sum_is_zero_on_initialization():
+    calculator = MemoryCalculator()
+    assert calculator.sum() == 0
 
-def test_lista_com_valores():
-    numeros = [1, 2, 3, 4, 5]
-    resultado = calcular_media(numeros)
-    assert resultado == 3.0
+def test_sum_one_number():
+    calculator = MemoryCalculator()
+    calculator.add(5)
+    assert calculator.sum() == 5
 
-def test_lista_com_valor_negativo():
-    numeros = [-1, 2, 3, 4, 5]
-    resultado = calcular_media(numeros)
-    assert resultado == 2.6
+def test_sum_two_numbers():
+    calculator = MemoryCalculator()
+    calculator.add(5)
+    calculator.add(10)
+    assert calculator.sum() == 15
+
+def test_sum_is_zero_after_calling_sum():
+    calculator = MemoryCalculator()
+    calculator.add(5)
+    calculator.sum()  # resultado: 5
+    assert calculator.sum() == 0
+
+def test_sum_numbers_and_save_last_sum():
+    calculator = MemoryCalculator()
+    calculator._save_last_sum = True
+    calculator.add(5)
+    calculator.add(10)
+    calculator.sum()
+    calculator.add(15)
+    assert calculator.last_sum + calculator.sum() == 30
